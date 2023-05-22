e6dad412cf0fd2dc7b7946016d94ad5603dee2ca
tkellys <thamireskellysantos@gmail.com>
2023-05-21 21:34:32 -0300

    atividade de ksa finalizada

--- src/components/CepForm.jsx
+++ src/components/CepForm.jsx
@@ -1,12 +1,18 @@
+import React from 'react'
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 
 
 export const CepForm = () => {
-
+//------ FAZENDO API DOS ESTADOS -------------------------------
     //useState é para inicializar o useEffect começa vazio e vai alimentando pelos options do MAP //setEstados sera a nova variavel preenchida por isso sera chamada la em baixo
     const [estados, setEstados] = useState([]);
+    const [estadoSelecionado, setEstadoSelecionado] = useState("");
+    const [cidade, setCidade] = useState([]);
+    const [cidadeSelecionado, setCidadeSelecionado] = useState("");
+    const [endereco, setEndereco] = useState("");
+    const [bairro, setBairro] = useState("");
 
     // a chamada da API é feita com uma const function dentro da function principal sempre.
     const buscarEstados = () => {
@@ -27,25 +33,55 @@ export const CepForm = () => {
                 console.log(error);
             });
         };
-        
+
+        const buscarCidades = () => {
+
+            //axios é p/ buscar API ao inves de fetch, nao esquecer de importar o axios la em cima
+            axios.get("https://servicodados.ibge.gov.br/api/v1/localidades/municipios")
+                .then((response) => {
+    
+                    // usaremos o MAP para fazer a lista aparecer nas options dos estados
+                    let arrayCidades = response.data.map((cidade, index) => {
+                        return (<option key={index} value={cidade.nome}> {cidade.nome}</option>)
+                    })
+                    //agora chama 'cidades' isso aqui na parte html do nosso form onde seria as options
+                    setCidade(arrayCidades)
+                })
+                .catch(function (error) {
+    
+                    console.log(error);
+                });
+            };
+ //------------- FAZENDO API DO CEP -------------------------------       
 //Aqui vamos carregar os dados atraves do cep para completar os campos automaticos
 // evento é pra comunicar com o onBlur la embaixo
             const BuscarEnderecoPorCep = (evento) => { 
-                
                 let cep = evento.target.value;
-              
-                axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-                .then(function (response){
+
+        //validaçao do formulario com regras do Regex (tudo q nao for numero sera substituido por vazio)
+                let cepvalidado = cep.replace(/[^0-9]/g, '');
+                  if(cepvalidado.length !== 8){
+                    return;}
                     
-                    console.log(response.data)
-                })
+                axios.get(`https://viacep.com.br/ws/${cepvalidado}/json/`)
+                .then((response) => {
+                    console.log(response, response.data);
+                    setEndereco(response.data.logradouro);
+                    setBairro(response.data.bairro);
+                    setEstadoSelecionado(response.data.uf);
+                    setCidadeSelecionado(response.data.localidade);
+                });
+            }
               
               
     //useEfect que vai chamar nosso get api
     useEffect(() => {
-        buscarEstados()
-    }, []) // colchete vazio para nao ter loop infinito
+        buscarEstados();
+        buscarCidades();
+    }, []); // colchete vazio para nao ter loop infinito
 
+    
+//--------------- AQUI É O FORMULARIO DO ENDEREÇO ----------------------
     return (
         <div className="container">
             <h1>Buscar Endereço com CEP</h1>
@@ -56,19 +92,21 @@ export const CepForm = () => {
                     <label htmlFor="inputCep"
                         className="form-label">
                         Cep</label>
-                    <input type="cep"
-                        onBlur={BuscarEnderecoPorCep} // toda chamada on precisa de um evento.target pra recuperar o valor aqui
+                    <input type="text"
+                        onBlur={(evento) => BuscarEnderecoPorCep(evento)} // toda chamada on precisa de um evento.target pra recuperar o valor aqui
                         className="form-control"
-                        id="cepId">
+                        id="cepId"
+                        >
                     </input>
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="inputEndereço4"
                         className="form-label">
                         Endereço</label>
-                    <input type="Endereço"
+                    <input type="text"
                         className="form-control"
-                        id="inputEndereço4">
+                        id="endereco"
+                        value={endereco}>
                     </input>
                 </div>
                 <div className="col-12">
@@ -87,7 +125,8 @@ export const CepForm = () => {
                         Bairro</label>
                     <input type="text"
                         className="form-control"
-                        id="inputBairro">
+                        id="bairro"
+                        value={bairro}>
                     </input>
                 </div>
 
@@ -96,7 +135,8 @@ export const CepForm = () => {
                         className="form-label">
                         Estado</label>
                     <select id="inputEstado"
-                        className="form-select">
+                        className="form-select"
+                       value={estadoSelecionado}>
                         <option selected>Selecione...</option>
                         {estados}
                     </select>
@@ -107,10 +147,11 @@ export const CepForm = () => {
                     <label htmlFor="inputCidade"
                         className="form-label">
                         Cidade</label>
-                    <select id="inputEstado"
-                        className="form-select">
+                    <select id="cidade"
+                        className="form-select"
+                        value={cidadeSelecionado}>
                         <option selected>Selecione...</option>
-                        <option>...</option>
+                     {cidade}
                     </select>
                 </div>
 
@@ -121,4 +162,4 @@ export const CepForm = () => {
             </form>
         </div>
     )
-    }}
+    }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 import { CepForm } from './components/CepForm.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-   
     <CepForm/>
+   
 
     </>
 )
